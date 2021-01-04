11d2b67af628c70217c7580952db094ac8c8b0d7
Neila Damak <neila.damak@supcom.tn>
2021-01-04 14:41:56 +0100

    nekhdmo ala rwehna

--- lib/screens/Main/settings.dart
+++ lib/screens/Main/settings.dart
@@ -1,6 +1,8 @@
+
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:job_finder/config/FontsConstants.dart';
 import 'package:job_finder/config/Palette.dart';
 import 'package:job_finder/screens/auth/auth.dart';
@@ -12,8 +14,8 @@ class SettingsPage extends StatefulWidget {
   SettingsPage({Key key}) : super(key: key);
 
   static MaterialPageRoute get route => MaterialPageRoute(
-        builder: (context) => SettingsPage(),
-      );
+    builder: (context) => SettingsPage(),
+  );
 
   Future<void> signOut() async {
     try {
@@ -36,117 +38,180 @@ class _SettingsPageState extends State<SettingsPage> {
   @override
   Widget build(BuildContext context) {
     final databaseReference =
-        FirebaseFirestore.instance.collection("users_data");
+    FirebaseFirestore.instance.collection("users_data");
     final litUser = context.getSignedInUser();
     String uid = "";
     litUser.when((user) => uid = user.uid, empty: () {}, initializing: () {});
 
     return new Scaffold(
       backgroundColor: Palette.powderBlue,
-      body: Column(
-        children: [
-          SizedBox(height: 50.0),
-          Row(children: [
-            Icon(
-              Icons.account_circle,
-            ),
-            SizedBox(width: 5.0),
-            RaisedButton(
-              child: Text("Modify your profile"),
-              onPressed: () {
-                showMaterialModalBottomSheet(
-                    context: context,
-                    builder: (BuildContext context) {
-                      return StatefulBuilder(builder:
-                          (BuildContext context, StateSetter setState) {
-                        return Padding(
-                          padding: const EdgeInsets.all(20.0),
-                          child: Column(children: [
-                            TextField(
-                              controller: myName,
-                              //to get the info in myText : myText.text
+      body: Padding(
+        padding: EdgeInsets.all(20.0),
+        child: Align(
+          child: Column(
+              children: [
+                SizedBox(height: 200.0),
+                FlatButton(
+                  onPressed: () {
+                    showMaterialModalBottomSheet(
+                        context: context,
+                        builder: (BuildContext context) {
+                          return StatefulBuilder(builder:
+                              (BuildContext context, StateSetter setState) {
+                            return Padding(
+                              padding: const EdgeInsets.all(30.0),
+                              child: Container(
+                                height : 300.0,
+                                child: Column(children: [
+                                  Text('Settings',
+                                    style: TextStyle(
+                                      fontSize: 25.0,
+                                    ),),
+                                  SizedBox(height : 30),
+                                  TextField(
+                                    controller: myName,
+                                    //to get the info in myText : myText.text
 
-                              cursorColor: Colors.black,
-                              decoration: InputDecoration(
-                                icon: Icon(
-                                  Icons.home,
-                                  size: 20.0,
-                                  color: Colors.black,
-                                ),
-                                border: InputBorder.none,
-                                hintText: "Enter your new name",
-                                hintStyle: kSubtitleStyle.copyWith(
-                                  color: Palette.navyBlue,
-                                ),
-                              ),
-                            ),
-                            SizedBox(
-                              height: 10.0,
-                            ),
-                            RaisedButton(
-                                child: Text('Submit'),
-                                onPressed: () {
-                                  setState(() {
-                                    newName = myName.text;
-                                    setData(databaseReference, uid, newName);
-                                  });
-                                })
-                          ]),
-                        );
-                      });
-                    });
-              },
-            ),
-          ]),
-          RaisedButton(
-            onPressed: () {
-              showMaterialModalBottomSheet(
-                  context: context,
-                  builder: (BuildContext context) {
-                    return
-                      Padding(
-                        padding: const EdgeInsets.all(30.0),
-                        child: Column(
-                            children: [
-                              InkWell(
-                                onTap: () {
-                                  Navigator.push(
-                                    context,
-                                    MaterialPageRoute(
-                                      builder: (context) => Tag(
-                                        category: 'python',
+                                    cursorColor: Colors.black,
+                                    decoration: InputDecoration(
 
+                                      border: InputBorder.none,
+                                      hintText: "Enter your new name",
+                                      hintStyle: kSubtitleStyle.copyWith(
+                                        color: Palette.navyBlue,
                                       ),
                                     ),
-                                  );
-                                },
-                                child: Column(
-                                  children: [
-                                    SizedBox(height: 50,),
-                                    Tag(category : 'python'),
-                                    Tag(category : "JavaScript"),
-
-                                  ],
-                                ),
+                                  ),
+                                  SizedBox(
+                                    height: 30.0,
+                                  ),
+                                  RaisedButton(
+                                      child: Text('Save'),
+                                      onPressed: () {
+                                        setState(() {
+                                          newName = myName.text;
+                                          setData(databaseReference, uid, newName);
+                                        });
+                                      })
+                                ]),
                               ),
-                            ]
+                            );
+                          });
+                        });
+                  },
+
+                  child: Row(children: [
+                    Icon(
+                      Icons.account_circle,
+                    ),
+                    SizedBox(width: 10.0),
+                    Text("Account Settings",
+                      style: TextStyle(
+                        fontSize: 20.0,
+
+                      ),
+                    ),
+
+
+                    SizedBox(width : 95.0),
+                    Icon(
+                      Icons.arrow_forward_ios,
+                      size:20.0,
+                    ),
+                  ]),
+                ),
+                SizedBox(height:15),
+                FlatButton(
+                  onPressed: (){
+
+                    showMaterialModalBottomSheet(
+                        context: context,
+                        builder: (BuildContext context) {
+                          return
+                            Padding(
+                              padding: const EdgeInsets.all(30.0),
+                              child: Column(
+                                  children: [
+                                    InkWell(
+                                      onTap: () {
+                                        Navigator.push(
+                                          context,
+                                          MaterialPageRoute(
+                                            builder: (context) => Tag(
+                                              category: 'python',
+
+                                            ),
+                                          ),
+                                        );
+                                      },
+                                      child: Column(
+                                        children: [
+                                          Tag(category : 'python'),
+                                          Tag(category : "JavaScript"),
+
+                                        ],
+                                      ),
+                                    ),
+                                  ]
+                              ),
+                            );
+
+                        });
+
+                  },
+
+                  child: Row(
+                    children: [
+                      Icon(
+                        Icons.business_center,
+                      ),
+
+                      SizedBox(width : 10.0),
+                      Text('Job Categories',
+                        style : TextStyle(
+                          fontSize: 20.0,
                         ),
-                      );
+                      ),
 
-                  });
+                      SizedBox(width : 120.0),
+                      Icon(
+                        Icons.arrow_forward_ios,
+                        size:20.0,
+                      ),
+                    ],
+                  ),
+                ),
+                SizedBox(height:15),
 
-            },
+                FlatButton(
 
-            child: Text('Job Categories'),
+                  child: Row(
+                    children: [
+                      Icon(
+                        Icons.assignment_return,
+                      ),
+                      SizedBox(width : 10.0),
+                      Text('Sign out',
+                        style : TextStyle(
+                          fontSize: 20.0,
+                        ),),
+                      SizedBox(width : 175.0),
+                      Icon(
+                        Icons.arrow_forward_ios,
+                        size:20.0,
+                      ),
+                    ],
+                  ),
+                  onPressed: () {
+                    context.signOut();
+                    Navigator.of(context).push(AuthScreen.route);
+                  },
+                ),
+
+
+              ]
           ),
-          RaisedButton(
-            onPressed: () {
-              context.signOut();
-              Navigator.of(context).push(AuthScreen.route);
-            },
-            child: const Text('Sign out'),
-          ),
-        ],
+        ),
       ),
     );
   }
