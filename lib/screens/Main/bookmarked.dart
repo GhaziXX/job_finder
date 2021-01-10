c73b0b5e738dc7ad9d826f7a6142272afe348bf6
Neila Damak <neila.damak@supcom.tn>
2021-01-10 17:10:54 +0100

    FutureBuild fel bookmark

--- lib/screens/Main/bookmarked.dart
+++ lib/screens/Main/bookmarked.dart
@@ -1,6 +1,5 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
-import 'package:job_finder/config/Palette.dart';
 import 'package:job_finder/models/compay.dart';
 
 import 'package:job_finder/widgets/recent_job_card.dart';
@@ -13,41 +12,30 @@ class BookmarkedPage extends StatefulWidget {
   BookmarkedPage({Key key, this.faves}) : super(key: key);
 
   static MaterialPageRoute get route => MaterialPageRoute(
-        builder: (context) => BookmarkedPage(),
-      );
+    builder: (context) => BookmarkedPage(),
+  );
 
   @override
   _BookmarkedPageState createState() => _BookmarkedPageState();
 }
+Future<List<Company>> futureFavouriteBooks;
 
 class _BookmarkedPageState extends State<BookmarkedPage> {
   static List<String> books = new List();
+
   @override
-  Widget build(BuildContext context) {
+  void initState() {
+    super.initState();
     final databaseReference =
-        FirebaseFirestore.instance.collection("users_data");
+    FirebaseFirestore.instance.collection("users_data");
     final litUser = context.getSignedInUser();
     String uid = "";
     litUser.when((user) => uid = user.uid, empty: () {}, initializing: () {});
     getBooks(databaseReference, uid);
-    return new Scaffold(
-      backgroundColor: Colors.white10,
-      body: new Center(
-        child: ListView.builder(
-          itemCount: RecentJobCard.faves.length,
-          itemBuilder: (context, index) {
-            var recent = RecentJobCard.faves[index];
+    futureFavouriteBooks = getFaveBooks();
 
-            return Column(children: [
-              Container(
-                child: RecentJobCard(company: recent),
-              ),
-            ]);
-          },
-        ),
-      ),
-    );
   }
+
 
   void getBooks(CollectionReference f, String uid) async {
     await f.get().then((QuerySnapshot snapshot) {
@@ -64,4 +52,48 @@ class _BookmarkedPageState extends State<BookmarkedPage> {
       books = temp;
     });
   }
+
+
+
+
+  @override
+  Widget build(BuildContext context) {
+    final databaseReference =
+    FirebaseFirestore.instance.collection("users_data");
+    final litUser = context.getSignedInUser();
+    String uid = "";
+    litUser.when((user) => uid = user.uid, empty: () {}, initializing: () {});
+    getBooks(databaseReference, uid);
+    return new Scaffold(
+      backgroundColor: Colors.white10,
+      body: Column(
+          children :[ FutureBuilder<List<Company>>(
+              future: futureFavouriteBooks,
+              builder: (context, snapshot) {
+
+                return StatefulBuilder(builder:
+                    (BuildContext context,
+                    StateSetter setState) {
+                  return ListView.builder(
+                      itemCount: snapshot.data.length,
+                      scrollDirection: Axis.vertical,
+                      shrinkWrap: true,
+                      physics: ScrollPhysics(),
+                      itemBuilder: (context, index) {
+                        var recent = snapshot.data[index];
+                        return InkWell(
+
+                          child: RecentJobCard(
+                              company: recent,couleur: Colors.red),
+                        );
+                      });
+                });
+
+              })
+          ]
+      ),
+    );
+  }
+
+
 }
--- lib/widgets/recent_job_card.dart
+++ lib/widgets/recent_job_card.dart
@@ -5,84 +5,112 @@ import 'package:job_finder/models/compay.dart';
 import 'package:job_finder/screens/Main/bookmarked.dart';
 import 'package:lit_firebase_auth/lit_firebase_auth.dart';
 
-Color savedColor;
 
+
+// ignore: must_be_immutable
 class RecentJobCard extends StatefulWidget {
   final Company company;
+  Color couleur;
   static List<Company> faves = List();
+  static Future<List<Company>> faveBooks = List() as Future<List<Company>>;
   static List<String> identifier = List();
+  RecentJobCard({this.company,this.couleur});
 
-  RecentJobCard({this.company});
 
   @override
   _RecentJobCardState createState() => _RecentJobCardState();
 }
+List<Company> fetchFaveBooks() {
+  return RecentJobCard.faves;
+}
 
+Future <List<Company>> getFaveBooks() async {
+  List<Company> booksList = fetchFaveBooks();
+  return booksList;
+}
 class _RecentJobCardState extends State<RecentJobCard> {
-  @override
   bool isPressed = false;
-  void _increment() {
-    setState(() {
-      savedColor = Colors.red;
-    });
-  }
 
   @override
   Widget build(BuildContext context) {
     final databaseReference =
-        FirebaseFirestore.instance.collection("users_data");
+    FirebaseFirestore.instance.collection("users_data");
     final litUser = context.getSignedInUser();
     String uid = "";
     litUser.when((user) => uid = user.uid, empty: () {}, initializing: () {});
+
     return Card(
-      color: Colors.white,
-      elevation: 0.0,
-      margin: EdgeInsets.only(right: 18.0, top: 15.0),
-      shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.circular(12.0),
-      ),
-      child: ListTile(
-        leading: SizedBox(
-            height: 80,
-            width: 80,
-            child: Image(image: NetworkImage(widget.company.companyLogo))),
-        title: FittedBox(child: Text(widget.company.title, style: kTitleStyle)),
-        subtitle: FittedBox(
-          child: Text(
-            "${widget.company.company} • ${widget.company.time}",
-          ),
-        ),
-        trailing: IconButton(
-            icon: Icon(Icons.favorite),
-            color: isPressed ? Colors.red : Colors.grey,
-            onPressed: () {
-              setState(() {
-                isPressed = !isPressed;
-                if (isPressed == true) {
-                  RecentJobCard.faves.add(widget.company);
-                  RecentJobCard.identifier.add(widget.company.id);
-                }
-                if (isPressed == false) {
-                  RecentJobCard.faves.remove(widget.company);
-                  RecentJobCard.identifier.remove(widget.company.id);
-                }
-                getData(databaseReference, uid, RecentJobCard.identifier);
-                print(RecentJobCard.faves);
-              });
-              return InkWell(
-                onTap: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) => BookmarkedPage(
-                        faves: RecentJobCard.faves,
-                      ),
-                    ),
-                  );
-                },
-              );
-            }),
-        isThreeLine: true,
+
+      child: StatefulBuilder(
+          builder: (context, setState) {
+            return Card(
+              color: Colors.white,
+              elevation: 0.0,
+              margin: EdgeInsets.only(right: 18.0, top: 15.0),
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(12.0),
+              ),
+              child: ListTile(
+
+                leading: SizedBox(
+                    height: 80,
+                    width: 80,
+                    child: Image(
+                        image: NetworkImage(widget.company.companyLogo))),
+                title: FittedBox(
+                    child: Text(widget.company.title, style: kTitleStyle)),
+                subtitle: FittedBox(
+                  child: Text(
+                    "${widget.company.company} • ${widget.company.time}",
+                  ),
+                ),
+                trailing: IconButton(
+                    icon: Icon(Icons.favorite),
+                    color: widget.couleur,
+
+                    onPressed:  () {
+
+                      setState(() {
+                        isPressed = !isPressed;
+
+
+                        if (isPressed == true) {
+                          widget.couleur = Colors.red;
+
+                          RecentJobCard.faves.add(widget.company);
+                          RecentJobCard.identifier.add(widget.company.id);
+                        }
+                        if (isPressed == false) {
+                          widget.couleur = Colors.grey;
+
+                          RecentJobCard.faves.remove(widget.company);
+
+                          RecentJobCard.identifier.remove(widget.company.id);
+                        }
+
+                        getData(
+                            databaseReference, uid, RecentJobCard.identifier);
+                        print(RecentJobCard.faves);
+                      });
+                      return InkWell(
+                        onTap: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) =>
+                                  BookmarkedPage(
+                                    faves: RecentJobCard.faves,
+                                  ),
+                            ),
+                          );
+                        },
+                      );
+                    }),
+                isThreeLine: true,
+              ),
+            );
+
+          }
       ),
     );
   }
